3ffcc1e54b64718b55ba327df535562676d16415
Picaljean313 <nony.adrien@gmail.com>
2022-10-26 17:35:44 +0200

    add of createOneComment function in controllers/comments file, and add of validation/data/createOneComment file

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -1,7 +1,35 @@
 const CommentsModel = require ('../models/Comments');
+const PostsModel = require ('../models/Posts');
+const createOneComment = require('../validation/data/createOneComment');
+const rules = require('../validation/rules');
+const functions = require('../functions');
 
-exports.createOneComment = (req, res, next) => {
+exports.createOneComment = async function (req, res, next) {
+  const validCommentJson = rules.valid(createOneComment.commentJsonDataToValidate, req.body); 
+  if(!validCommentJson) return functions.response(res, 400);
 
+  let post;
+  try {
+    post = await PostsModel.findOne({ _id : req.body.postId });
+  } catch {
+    console.log("Can't find post.");
+    return functions.response(res, 500);
+  }
+  if (post === null) return functions.response(res, 400);
+
+  const commentCreated = new CommentsModel ({
+    content : req.body.content,
+    postId : req.body.postId,
+    userId : req.auth.userId
+  });
+  try {
+    await commentCreated.save()
+  } catch {
+    console.log("Can't save comment.");
+    return functions.response(res, 500);
+  }
+  
+  return res.status(201).json({ message : "Comment created." });
 };
 
 exports.getAllComments = (req, res, next) => {
