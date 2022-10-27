02b7897c6fb3660b47f821d0d8136d2f3baa642f
Picaljean313 <nony.adrien@gmail.com>
2022-10-27 16:02:28 +0200

    Add of getAllReports function in controllers/reports file

--- backend/controllers/reports.js
+++ backend/controllers/reports.js
@@ -73,8 +73,75 @@ exports.createOneReport = async function (req, res, next) {
   return res.status(201).json({ message : "report created." });
 };
 
-exports.getAllReports = (req, res, next) => {
+exports.getAllReports = async function (req, res, next) {
+  const allowedQueries = ["minDate", "maxDate", "limit", "sort", "fromUserId", "fromPostId", "fromCommentId"]; 
+  const reqQueriesObject = url.parse(req.url, true).query;
+  const reqQueriesKeys = Object.keys(reqQueriesObject);
+  const invalidReqQueries = reqQueriesKeys.map(x => allowedQueries.includes(x)).includes(false);
+  if (invalidReqQueries) return functions.response(res, 400);
 
+  const validParams = rules.valid(reqQueries.reqQueriesToValidate, reqQueriesObject);
+  if (!validParams) return functions.response(res, 400);
+
+  const minDate = req.query.minDate ? Date.parse(req.query.minDate) : 0;
+  const maxDate = req.query.maxDate ? Date.parse(req.query.maxDate) : Date.now();
+  const limit = req.query.limit ? Number(req.query.limit) : null;
+  const sort = req.query.sort ? req.query.sort : null;
+  const fromUserId = req.query.fromUserId;
+  const fromPostId = req.query.fromPostId;
+  const fromCommentId = req.query.fromCommentId;
+  const searchParam = {};
+
+  if (fromPostId && fromCommentId) return functions.response(res, 400);
+
+  if (fromUserId) {
+    let user;
+    try {
+      user = await UsersModel.findOne({ _id : fromUserId });
+    } catch {
+      console.log("Can't find user.");
+      return functions.response(res, 500);
+    }
+    if (user === null) return functions.response(res, 400);
+
+    searchParam.userId = fromUserId;
+  }
+
+  if (fromPostId) {
+    let post;
+    try {
+      post = await PostsModel.findOne({ _id : fromPostId });
+    } catch {
+      console.log("Can't find post.");
+      return functions.response(res, 500);
+    }
+    if (post === null) return functions.response(res, 400);
+
+    searchParam.postId = fromPostId;
+  }
+
+  if (fromCommentId) {
+    let comment;
+    try {
+      comment = await CommentsModel.findOne({ _id : fromCommentId });
+    } catch {
+      console.log("Can't find comment.");
+      return functions.response(res, 500);
+    }
+    if (comment === null) return functions.response(res, 400);
+
+    searchParam.commentId = fromCommentId;
+  }
+
+  let reports;
+  try {
+    reports = await ReportsModel.find(searchParam).sort({creationDate : sort}).where("creationDate").gte(minDate).lte(maxDate).limit(limit).lean();
+  } catch {
+    console.log("Can't find reports.");
+    return functions.response(res, 500);
+  } 
+
+  return res.status(200).json(reports);
 };
 
 exports.deleteAllReports = (req, res, next) => {
